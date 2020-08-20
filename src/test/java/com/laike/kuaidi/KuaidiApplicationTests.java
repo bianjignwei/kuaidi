3594dba95e60a6ef46368e0051a7e7f86db1395f
魏子皓 <575852846@qq.com>
2020-08-20 09:07:29 +0800

    初次上传

---
+++ src/test/java/com/laike/kuaidi/KuaidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laike.kuaidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuaidiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
